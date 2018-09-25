74d33d8026f3b0105260a8551d6669f7812d6168
MehdiRouis <mehdi.rouis.1@laposte.net>
2018-09-25 14:08:43 +0200

    Optimisation de l'index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,27 @@
 M.toast({html: 'Chargement en cours...'});
 $(document).ready(function() {
-  $(jQuery).ready(function() {
-    setTimeout(function() {
-      M.Toast.dismissAll();
-      M.toast({html: 'Chargement terminé! :3', displayLength: 1000});
-      $('.preloader-background').fadeOut('slow');
-      $('.preloader-wrapper').fadeOut();
-    }, 1000);
-    $('.sidenav').sidenav({edge: 'right'});
-    $('.scrollspy').scrollSpy({activeClass : ''});
+  setTimeout(function() {
+    M.Toast.dismissAll();
+    M.toast({html: 'Chargement terminé! :3', displayLength: 1000});
+    $('.preloader-background').fadeOut('slow');
+    $('.preloader-wrapper').fadeOut();
+  }, 1000);
+  $('.sidenav').sidenav({edge: 'right'});
+  $('.scrollspy').scrollSpy({activeClass : ''});
 
-    $('#b-story').click(function() {
-      document.getElementById('b-story').className = 'active';
-      document.getElementById('b-logs').className = '';
-      document.getElementById('b-skills').className = '';
-    });
-    $('#b-logs').click(function() {
-      document.getElementById('b-story').className = '';
-      document.getElementById('b-logs').className = 'active';
-      document.getElementById('b-skills').className = '';
-    });
-    $('#b-skills').click(function() {
-      document.getElementById('b-story').className = '';
-      document.getElementById('b-logs').className = '';
-      document.getElementById('b-skills').className = 'active';
-    });
+  $('#b-story').click(function() {
+    document.getElementById('b-story').className = 'active';
+    document.getElementById('b-logs').className = '';
+    document.getElementById('b-skills').className = '';
+  });
+  $('#b-logs').click(function() {
+    document.getElementById('b-story').className = '';
+    document.getElementById('b-logs').className = 'active';
+    document.getElementById('b-skills').className = '';
+  });
+  $('#b-skills').click(function() {
+    document.getElementById('b-story').className = '';
+    document.getElementById('b-logs').className = '';
+    document.getElementById('b-skills').className = 'active';
   });
 });
