9e4427d1826538ea5d196c203025e841dd0adbcb
MehdiRouis <mehdi.rouis.1@laposte.net>
2019-01-22 09:12:33 +0100

    Correction d'un script en JS

--- assets/js/step-by-step.js
+++ assets/js/step-by-step.js
@@ -1,54 +1,54 @@
 "use strict";
 
-            /*
-             * Plugin jQuery dvjhEcrit
-             *
-             * Daniel Hagnoul
-             *
-             * Code v1.0.0 2012-12-01
-             * ------------------------------------------
-             * Usage :
-             *
-             * $( selector ).dvjhEcrit({
-             *     "text" : "le texte à écrire"
-             * });
-             *
-             * Options disponibles, réglages par défaut :
-             *     "backgroundColor" : "#ffffff",
-             *     "color" :"#000000",
-             *     "fontFamily" : "cursive",
-             *     "fontSize" : "1.2em",
-             *     "fontWeight" : "normal",
-             *     "lineHeight" : "1.5em",
-             *     "letterSpacing" : "0.15em",
-             *     "interval" : 100
-             */
-(function(){
+/*
+ * Plugin jQuery dvjhEcrit
+ *
+ * Daniel Hagnoul
+ *
+ * Code v1.0.0 2012-12-01
+ * ------------------------------------------
+ * Usage :
+ *
+ * $( selector ).dvjhEcrit({
+ *     "text" : "le texte à écrire"
+ * });
+ *
+ * Options disponibles, réglages par défaut :
+ *     "backgroundColor" : "#ffffff",
+ *     "color" :"#000000",
+ *     "fontFamily" : "cursive",
+ *     "fontSize" : "1.2em",
+ *     "fontWeight" : "normal",
+ *     "lineHeight" : "1.5em",
+ *     "letterSpacing" : "0.15em",
+ *     "interval" : 100
+ */
+( function( $ ){
   $.fn.dvjhEcrit = function( options ){
     var opts = $.extend( true, {}, $.fn.dvjhEcrit.defaults, options );
 
     return this.each( function( i, item ){
       var interval = parseInt( opts.interval , 10) || 100,
-      tabChaine = $( item ).text().split(''),
-      longueur = tabChaine.length,
-      lettre = [];
+          tabChaine = $( item ).text().split(''),
+          longueur = tabChaine.length,
+          lettre = [];
       $(item).text('');
 
       for( var k = 0; k < longueur; k++ ){
         lettre.push(
-          $( "<span/>", {
-            "css" : {
-              "display" : "none",
-              "backgroundColor" : opts.backgroundColor,
-              "color" : opts.color,
-              "fontFamily" : opts.fontFamily,
-              "fontSize" : opts.fontSize,
-              "fontWeight" : opts.fontWeight,
-              "lineHeight" : opts.lineHeight,
-              "letterSpacing" : opts.letterSpacing
-            },
-            "text" : tabChaine[ k ]
-          })
+            $( "<span/>", {
+              "css" : {
+                "display" : "none",
+                "backgroundColor" : opts.backgroundColor,
+                "color" : opts.color,
+                "fontFamily" : opts.fontFamily,
+                "fontSize" : opts.fontSize,
+                "fontWeight" : opts.fontWeight,
+                "lineHeight" : opts.lineHeight,
+                "letterSpacing" : opts.letterSpacing
+              },
+              "text" : tabChaine[ k ]
+            })
         );
       }
 
@@ -72,4 +72,4 @@
     "letterSpacing" : "0.15em",
     "interval" : 100,
   };
-});
+})(jQuery);
